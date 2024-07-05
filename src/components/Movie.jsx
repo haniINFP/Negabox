3f94fdfcf11d227a8e86663eb3dee8510ad36f11
junsung12 <choikukh19@naver.com>
2024-07-05 14:49:44 +0900

    rmfwk

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -5,11 +5,12 @@ const IMG_BASE_URL = "https://image.tmdb.org/t/p/w1280/";
 
 
 
-export default function Movie({ id, title, poster_path, popularity }) {
+export default function Movie({ id, title, poster_path, popularity, overview }) {
   return (
       <Link to={`/detail/${id}`}>
         <div className="movie-container">
           <img src={IMG_BASE_URL + poster_path} alt="영화포스터" width={'400px'} height={'300x'} />
+          <div className="overview">{overview}</div>
           <div className="movie-info">
             <div className="title">{title}</div>
             <div className="popularity">{popularity}</div>
